65eff88aaecc7314849eab81043702fe8963a972
Nikola Selic <selich.work@gmail.com>
2019-08-14 20:55:35 +0200

    refactoring Draggable

--- src/App.js
+++ src/App.js
@@ -1,81 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Draggable from "./components/Draggable"
 import "./App.css";
 
-const throttle = (f) => {
-  let token = null, lastArgs = null;
-  const invoke = () => {
-      f(...lastArgs);
-      token = null;
-  };
-  const result = (...args) => {
-      lastArgs = args;
-      if (!token) {
-          token = requestAnimationFrame(invoke);
-      }
-  };
-  result.cancel = () => token && cancelAnimationFrame(token);
-  return result;
-};
-
-class Draggable extends React.PureComponent {
-  _relX = 0;
-  _relY = 0;
-  _ref = React.createRef();
-
-  _onMouseDown = (event) => {
-      if (event.button !== 0) {
-          return;
-      }
-      const {scrollLeft, scrollTop, clientLeft, clientTop} = document.body;
-      const {left, top} = this._ref.current.getBoundingClientRect();
-      this._relX = event.pageX - (left + scrollLeft - clientLeft);
-      this._relY = event.pageY - (top + scrollTop - clientTop);
-      document.addEventListener('mousemove', this._onMouseMove);
-      document.addEventListener('mouseup', this._onMouseUp);
-      event.preventDefault();
-  };
-
-  _onMouseUp = (event) => {
-      document.removeEventListener('mousemove', this._onMouseMove);
-      document.removeEventListener('mouseup', this._onMouseUp);
-      event.preventDefault();
-  };
-
-  _onMouseMove = (event) => {
-      this.props.onMove(
-          event.pageX - this._relX,
-          event.pageY - this._relY,
-      );
-      event.preventDefault();
-  };
-
-  _update = throttle(() => {
-      const {x, y} = this.props;
-      this._ref.current.style.transform = `translate(${x}px, ${y}px)`;
-  });
-
-  componentDidMount() {
-      this._ref.current.addEventListener('mousedown', this._onMouseDown);
-      this._update();
-  }
-
-  componentDidUpdate() {
-      this._update();
-  }
-
-  componentWillUnmount() {
-      this._ref.current.removeEventListener('mousedown', this._onMouseDown);
-      this._update.cancel();
-  }
-
-  render() {
-      return (
-          <div className="draggable" ref={this._ref}>
-              {this.props.children}
-          </div>
-      );
-  }
-}
 
 class App extends React.PureComponent {
   state = {
@@ -84,7 +10,6 @@ class App extends React.PureComponent {
   };
 
   _move = (x, y) => this.setState({x, y});
-
 
   render() {
       const {x, y} = this.state;
